b45fc100214b61be1168553565bc04f13df3f4a8
Thangaraju <b.thangaraju@iiitb.ac.in>
2021-03-12 05:40:20 +0000

    GitHub hook trigger for GITScm polling

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,7 +1,5 @@
 #! /usr/bin/python3
 # This Phython program will print Hellow World...
-print("Hellow World...\n")
-print("Hellow World...\n")
-print("Hellow World...\n")
+print("Hello World Demo for GitHub hook trigger for GITScm polling...\n")
 
 
