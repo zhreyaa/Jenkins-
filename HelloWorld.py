64bfab11c0dd2f26cf6f42a9ba26e1d722857d80
Thangaraju <b.thangaraju@iiitb.ac.in>
2021-03-19 08:52:35 +0000

    second

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,15 +1,4 @@
 #! /usr/bin/python3
 # This Phython program will print Hellow World...
-<<<<<<< HEAD
 print("Hello World Demo for GitHub hook trigger for GITScm polling...\n")
-print("Hello World Demo for GitHub hook trigger for GITScm polling...\n")
-=======
-
-print("Hello World Demo for GitClone Pipeline...\n")
-
-
-
-
->>>>>>> 41c44c51691fc2f9ca2697c48cbbaad6d5760f2f
-
 
