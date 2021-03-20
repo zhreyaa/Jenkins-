871758654eeb1c6b4946fccbd12397b00fa28a42
Prof. B Thangaraju <35483639+BThangaraju@users.noreply.github.com>
2021-03-20 14:28:03 +0530

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,4 +2,4 @@
 # This Phython program will print Hellow World...
 print("Hello World Demo for GitHub hook trigger for GITScm polling...\n")
 print("Hello World Demo for GitHub hook trigger for GITScm polling...\n")
-
+print("Hello GitTrigger Project\n");
