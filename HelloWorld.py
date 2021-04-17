4857a2ea939253fe899b389bf9ce693188ead149
Prof. B Thangaraju <35483639+BThangaraju@users.noreply.github.com>
2021-04-17 16:35:22 +0530

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python3
 # This Phython program will print Hellow World...
 print("Hello World Demo with GitHub Integration...\n")
+print("Hello World Demo with GitHub Integration...\n")
 
 
 
