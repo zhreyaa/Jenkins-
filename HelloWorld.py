e1f1bb5e409103c41308bee8bda0b5b90686902c
Prof. B Thangaraju <35483639+BThangaraju@users.noreply.github.com>
2021-06-03 10:49:05 +0530

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,7 +2,6 @@
 # This Phython program will print Hellow World...
 print("Hello World Demo with GitHub Integration with WEBHOOK...\n")
 print("Hello World Demo with GitHub Integration with WEBHOOK... NEW...\n")
-print("Hello World Demo with GitHub Integration with WEBHOOK... NEW...\n")
 
 
 
