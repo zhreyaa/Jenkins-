cb466a7bb23af604fee623b494865ca4ba8ced99
Prof. B Thangaraju <35483639+BThangaraju@users.noreply.github.com>
2021-07-25 10:44:38 +0530

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,8 +1,6 @@
 #! /usr/bin/python3
 # This Phython program will print Hellow World...
-
-print("Hello World Demo for GITSCM ...\n")
-print("Hello World Demo for GITSCM ...\n")
+print("Hello World Demo for GITSCM Demo...\n")
 
 
 
