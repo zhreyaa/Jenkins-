880970c109a05962efa709407672c013835c1c44
Prof. B Thangaraju <35483639+BThangaraju@users.noreply.github.com>
2021-08-22 14:50:43 +0530

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,6 +1,8 @@
 #! /usr/bin/python3
 # This Phython program will print Hellow World...
 print("Hello World Demo for Periodic Demo...\n")
+print("Hello World Demo for Periodic Demo...\n")
+print("Hello World Demo for Periodic Demo...\n")
 
 
 
