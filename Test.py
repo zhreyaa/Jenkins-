564d4e3866c216a9e699385aaae8162edb26131f
Prof. B Thangaraju <35483639+BThangaraju@users.noreply.github.com>
2021-10-17 11:51:32 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -9,7 +9,7 @@ class TestSum(unittest.TestCase):
         """
         Test case to add two numbers
         """
-        data = [22, 32]
+        data = [23, 32]
         result = summation(data)
         self.assertEqual(result, 55)
 
