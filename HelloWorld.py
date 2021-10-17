908872a17cf6fcbea640171d0acc7665feafbe56
Prof. B Thangaraju <35483639+BThangaraju@users.noreply.github.com>
2021-10-17 11:21:17 +0530

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,7 +1,5 @@
 #! /usr/bin/python3
 # This Phython program will print Hellow World...
-print("Hello World Demo for Webhook Demo...\n")
-print("Hello World Demo for Webhook Demo...\n")
 print("Hello World Demo for Webhook Demo...NEW\n")
 
 
