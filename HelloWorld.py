6c9e265024207b01bb9943e528b3771b93ac49c1
Prof. B Thangaraju <35483639+BThangaraju@users.noreply.github.com>
2023-01-24 10:16:28 +0530

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,9 +1,7 @@
 #! /usr/bin/python3
 # This Phython program will print Hellow World...
 print("Hello World Demo...24 Jan 2023...new \n")
-print("Hello World Demo...24 Jan 2023...new \n")
-print("Hello World Demo...24 Jan 2023...new \n")
-print("Hello World Demo...24 Jan 2023...new \n")
+
 
 
 
